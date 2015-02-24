077311feb272028643e68b87a34f6766d7688fa1
louisewiksten <louisesmw@gmail.com>
2015-02-24 09:23:04 +0100

    Edited
    d

--- KEX2015/src/KEX2015/src/diceClass/Dice.java
+++ KEX2015/src/KEX2015/src/diceClass/Dice.java
@@ -1,7 +1,23 @@
 package KEX2015.src.diceClass;
+
+/**
+ * Creates a Dice class that represents a Dice and implements the different functions of
+ * the dice.
+ * @author D.Jendeberg and L.Wiksten
+ *
+ */
 public class Dice {
 	public int score;
+	
+	public Dice(){
+		
+	}
+	
 	public void roll(){
 		score = 1;
+	}
+	
+	public int getScore(){
+		return score;
 	}
 }
