f154ca92ac156e8899ee402e1e56ba5c1c15f8f2
louisewiksten <louisesmw@gmail.com>
2015-02-24 10:11:43 +0100

    ScoreCard
    A few small changes in the score card class, such as getTotalScore and
    setScore.

--- KEX2015/src/yahtzee/ScoreCard.java
+++ KEX2015/src/yahtzee/ScoreCard.java
@@ -13,10 +13,26 @@ public class ScoreCard {
 	public ScoreCard(){
 		results = new int[16]; //Change to n.o. lines
 		diceResults = new int[16][5];
+		sum = 0;
 	}
 	
 	public void setScore(int[] rolls, int row){
-		
+		int rowSum = 0;
+		for(int i = 0; i < 16; i++){
+			diceResults[row][i] = rolls[i];
+		}
+		sum += rowSum;
+		results[row] = rowSum;
+	}
+	
+	public int getTotalScore(){
+		int tempSum = 0;
+		for (int i = 0; i < 6; i++){
+			tempSum = results[i];
+		}
+		if(tempSum >= BONUSLIMIT)
+			sum += BONUSLIMIT;
+		return sum;
 	}
 	
 }
