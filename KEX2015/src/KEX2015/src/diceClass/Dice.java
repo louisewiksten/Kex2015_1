b3e80a9ef353cd69d8b85c91741d935b9857ab1a
Jendeberg <daniel.jendeberg@gmail.com>
2015-02-24 09:33:01 +0100

    Dice class
    Finishing touches

--- KEX2015/src/KEX2015/src/diceClass/Dice.java
+++ KEX2015/src/KEX2015/src/diceClass/Dice.java
@@ -1,5 +1,5 @@
 package KEX2015.src.diceClass;
-
+import java.util.Random;
 /**
  * Creates a Dice class that represents a Dice and implements the different functions of
  * the dice.
@@ -7,17 +7,31 @@ package KEX2015.src.diceClass;
  *
  */
 public class Dice {
-	public int score;
+	private int score;
 	
 	public Dice(){
-		
+		this.roll();
 	}
 	
+	/**
+	 * Simulates rolling the dice.
+	 */
 	public void roll(){
-		score = 1;
+		Random rng = new Random();
+		score = rng.nextInt(5)+1;
 	}
 	
+	/**
+	 * Returns number facing upwards on dice.
+	 */
 	public int getScore(){
 		return score;
 	}
+	
+	//Below method for testing purposes
+	/*public static void main(String args[]){
+		Dice d = new Dice();
+		d.roll();
+		System.out.println(d.getScore());
+	}*/
 }
