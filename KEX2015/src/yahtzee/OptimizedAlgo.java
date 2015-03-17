36ac9e178eec3ed785112df760092ad42ce9ecee
Jendeberg <daniel.jendeberg@gmail.com>
2015-03-17 20:59:57 +0100

    Optimized algorithm start
    Started figuring out how to make the optimal algorithm

--- KEX2015/src/yahtzee/OptimizedAlgo.java
+++ KEX2015/src/yahtzee/OptimizedAlgo.java
@@ -17,21 +17,65 @@ public class OptimizedAlgo {
 				playableScore[i]=false;
 			}
 		}
+		for(int i=2; i>0; i--){
+			evaluate(dices, playableScore, i);
+		}
 		
 	}
-	
-	public static void evaluate(Dice dices, boolean[] playableScore){
+	/**
+	 * Evaluates a set of dices an a list of playable scores in the scorecard.
+	 * The evaluation evaluates which score is most probable to be reached in a given set of
+	 * turns.
+	 * @param dices
+	 * @param playableScore
+	 */
+	public static void evaluate(Dice[] dices, boolean[] playableScore, int rollsLeft){
+		double[] expValues = new double[16];
+		int[] values = new int[5];
+		
+		//ones to sixes
+		for(int j = 1; j<7;j++){
+			for(int i=0;i<3;i++){
+			values[i]=j;
+			}
+			//calculates expected value of each category.
+			expValues[j] = probability(dices, values, rollsLeft)*score(j, values);
+		}
 		
 	}
 	
 	/**
 	 * Help method to calculate the distance from a list of dices to a 
 	 * set of dice scores.
-	 * @param dices
+	 * 
+	 * @param dices 
 	 * @param sc
 	 */
-	private static void probability(Dice dices, int[] scores){
+	private static double probability(Dice[] dices, int[] scores, int rollsLeft){
 		
+		
+		return 0;
 	}
-	
+	/**
+	 * 
+	 * 
+	 * @param category Category for which expected value is needed.
+	 * @return Returns score for supplied category.
+	 */
+	private static double score(int category, int[] values){
+		int ret = 0;
+		switch(category){
+		case(1):
+		case(2):
+		case(3):
+		case(4):
+		case(5):
+		case(6):
+			for(int value : values){
+				ret+=value;
+			}
+			return ret;
+		}
+		return 0;
+	}
 }
