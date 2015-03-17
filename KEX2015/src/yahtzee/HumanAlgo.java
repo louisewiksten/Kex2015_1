b0f144cf5335bcec1b1c0729f4864f3805b81aa8
lwiksten <lwiksten@kth.se>
2015-03-17 20:16:13 +0100

    New files
    Uploaded

---
+++ KEX2015/src/yahtzee/HumanAlgo.java
@@ -0,0 +1,40 @@
+package yahtzee;
+
+import java.util.Arrays;
+
+public class HumanAlgo {
+	/**
+	 * Play method to simulate a game of solitare yahtzee.
+	 *  
+	 * @param dices The the sides facing up on the dices.
+	 * @param sc The scorecard, i.e. current points and empty rows for not played.
+	 */
+	public static void play(Dice[] dices, ScoreCard sc){
+		//The value for turn will either be submitted or calculated,
+		//can easily be calculated by the scorecard.
+		for (int i = 0; i<2; i++){
+			evaluate(1, dices);
+		}
+		//Sets the score for a given row (turn).
+		int[] list = new int[5];
+		for(int i = 0; i < 5; i++)
+			list[i] = dices[i].getScore();
+		sc.setScore(list, 2);
+	}
+	/**
+	 * Method that evaluates a list of dices and rerolls the dices if needed.
+	 *
+	 * @param turn The number of turns into the game.
+	 * @param dices A list of Dices that can be rerolled if needed.
+	 */
+	public static void evaluate(int turn, Dice[] dices){
+		int [] scores;
+		boolean pair;
+		boolean trice;
+		boolean foursome;
+		switch(turn){
+		
+		}
+		
+	}
+}
---
+++ KEX2015/src/yahtzee/OptimizedAlgo.java
@@ -0,0 +1,15 @@
+package yahtzee;
+
+/**
+ * An algorithm that plays yahtzee optimally.
+ */
+public class OptimizedAlgo {
+
+	/**
+	 * Evaluates the score based on the given dices. 
+	 */
+	public static void play(Dice[] dices, ScoreCard score){
+		
+		
+	}
+}
