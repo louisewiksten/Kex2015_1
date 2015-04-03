c73e56fd64760b0b2bf77694bbe63c7167241427
lwiksten <lwiksten@kth.se>
2015-04-03 19:31:16 +0200

    Write test to file!
    edited the tests!

--- KEX2015/src/tests/PerformanceTestOptimizedHumanAlgo.java
+++ KEX2015/src/tests/PerformanceTestOptimizedHumanAlgo.java
@@ -1,8 +1,11 @@
 package tests;
 
+import java.io.FileWriter;
+
 import yahtzee.*;
 
 public class PerformanceTestOptimizedHumanAlgo {
+	
 	public static void main(String[] args){
 		final int runs = 100000; //The number of runs in this test. 
 		ScoreCard sc = new ScoreCard();
@@ -12,8 +15,10 @@ public class PerformanceTestOptimizedHumanAlgo {
 		int totalScore = 0;
 		int maxScore = 0;
 		int minScore = 330;
-		//int[] scores = new int[runs]; //Print to an excel file?
+		int[] scores = new int[runs]; //Print to an excel file?
+		int tempScore = 0;
 		for (int i = 0; i < runs; i++){
+			tempScore = 0;
 			for (int j = 0; j<15; j++){
 				OptimizedAlgo.play(dices, sc);
 				
@@ -21,11 +26,33 @@ public class PerformanceTestOptimizedHumanAlgo {
 					d.roll();
 				}
 			}
-			totalScore += sc.getTotalScore();
-			maxScore = Math.max(maxScore, sc.getTotalScore());
-			minScore = Math.min(minScore, sc.getTotalScore());
+			tempScore = sc.getTotalScore();
+			scores[i] = tempScore;
+			totalScore += tempScore;
+			maxScore = Math.max(maxScore, tempScore);
+			minScore = Math.min(minScore, tempScore);
 			sc = new ScoreCard();
 		}
+		
+		
+		/* ** Write to file** */
+		StringBuilder sb = new StringBuilder(""+scores[0]);
+		for(int i = 1; i < runs; i++){
+			sb.append("\n"+scores[i]);
+		}
+		try
+		{
+		    FileWriter writer = new FileWriter("oldOptimalAlgo.csv");
+		    writer.append(sb.toString());
+		    writer.flush();
+		    writer.close();
+		   
+		} catch(Exception e){
+			//File error.
+			 e.printStackTrace();
+		}
+		/* * * * * * * * * * */
+		
 		
 		System.out.println("During "+runs+" tests, the Optimized Algorithm got" +
 				" a total \nscore of: "+totalScore + " which means " + 
